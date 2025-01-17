923240cde143e8250b5ecc33d56622ef7d66f678
jian198001 <jian198001@163.com>
2025-01-18 00:42:01 +0800

    feat: 物流添加生产厂家

--- src/entity/Consume.ts
+++ src/entity/Consume.ts
@@ -4,34 +4,42 @@ import { BaseModel } from "../module/common/model/BaseModel";
 
 /**
  * 物料领用记录实体类
- * 
+ *
  * 该类用于定义物料领用记录的基本信息，包括员工ID、物料ID和购买数量。
  * 所有标识符名称均来自支付宝。
  */
 @Entity()
 export class Consume extends BaseModel {
-    /**
-     * 员工ID
-     * 
-     * 领用物料的员工的唯一标识符。
-     */
-    @Column({ nullable: true, comment: '员工ID', name: 'staff_id' })
-    public staffId: string;
+  /**
+   * 员工ID
+   *
+   * 领用物料的员工的唯一标识符。
+   */
+  @Column({ nullable: true, comment: "员工ID", name: "staff_id" })
+  public staffId: string;
 
-    /**
-     * 物料ID
-     * 
-     * 被领用物料的唯一标识符。
-     */
-    @Column({ nullable: true, comment: '物料ID', name: 'material_id' })
-    public materialId: string;
+  /**
+   * 物料ID
+   *
+   * 被领用物料的唯一标识符。
+   */
+  @Column({ nullable: true, comment: "物料ID", name: "material_id" })
+  public materialId: string;
 
-    /**
-     * 购买数量
-     * 
-     * 领用的物料数量。
-     */
-    @Column({ nullable: true, comment: '购买数量', type: 'double' })
-    @ApiProperty({ description: '购买数量,标识符名称来自支付宝' })
-    public quantity: number = 1;
+  /**
+   * 购买数量
+   *
+   * 领用的物料数量。
+   */
+  @Column({ nullable: true, comment: "购买数量", type: "double" })
+  @ApiProperty({ description: "购买数量,标识符名称来自支付宝" })
+  public quantity: number = 1;
+
+  /**
+   * 物料ID
+   *
+   * 被领用物料的唯一标识符。
+   */
+  @Column({ nullable: true, comment: "用途" })
+  public purpose: string;
 }
